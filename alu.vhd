6df5d3508e3f5a867bd2c970020d8c15b44994e0
Helge Rausch <helge@rausch.io>
2011-12-31 12:15:49 +0100

    adding ALU and tests

---
+++ alu.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:11:01 11/11/2011 
+-- Design Name: 
+-- Module Name:    alu - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity alu is
+	port( op_a, op_b, opc : in   STD_LOGIC_VECTOR (3 downto 0);
+		  status, result  : out  STD_LOGIC_VECTOR (3 downto 0));
+end alu;
+
+architecture Behavioral of alu is
+  
+begin
+
+	process ( op_a, op_b, opc )
+		variable carry : std_logic := '0';
+		variable zero  : std_logic := '0';
+		variable tmp   : unsigned (3 downto 0);
+	begin
+ 
+		status <= "0000";
+		carry := '0';
+		zero := '0';
+		
+		case opc is
+			
+			when "0000" | "0001" => -- ADD | ADDC
+				tmp := unsigned(op_a) + unsigned(op_b);
+				result <= std_logic_vector(tmp);
+				if tmp > 15 then
+					carry := '1';
+				end if;
+
+			when "0010" | "0011" => -- SUB | SUBC
+				result <= std_logic_vector(unsigned(op_a) - unsigned(op_b));
+				if unsigned(op_b) > unsigned(op_a) then
+					carry := '1';
+				end if;
+
+			when "0100" => -- AND
+				result <= op_a and op_b;
+
+			when "0101" => -- OR
+				result <= op_a or op_b;
+
+			when "0110" => -- XOR
+				result <= op_a xor op_b;
+
+			when "0111" => -- CMPA
+				result <= not(op_a);
+
+			when others => result <= "1111";
+		end case;
+
+		status(2) <= carry;
+		if tmp = 0 then
+			status(3) <= '1';
+		end if;
+
+	end process;
+
+end Behavioral;
+
---
+++ alu_test.vhd
@@ -0,0 +1,100 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   12:14:43 11/25/2011
+-- Design Name:   
+-- Module Name:   I:/VHDL/htwP/alu_test.vhd
+-- Project Name:  htwP
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: alu
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+USE ieee.numeric_std.ALL;
+ 
+ENTITY alu_test IS
+END alu_test;
+ 
+ARCHITECTURE behavior OF alu_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+	COMPONENT alu
+	PORT(
+		op_a   : IN  std_logic_vector(3 downto 0);
+		op_b   : IN  std_logic_vector(3 downto 0);
+		opc    : IN  std_logic_vector(3 downto 0);
+		status : OUT std_logic_vector(3 downto 0);
+		result : OUT std_logic_vector(3 downto 0)
+	);
+	END COMPONENT;
+
+
+	--Inputs
+	signal op_a : std_logic_vector(3 downto 0) := (others => '0');
+	signal op_b : std_logic_vector(3 downto 0) := (others => '0');
+	signal opc : std_logic_vector(3 downto 0) := (others => '0');
+
+	--Outputs
+	signal status : std_logic_vector(3 downto 0);
+	signal result : std_logic_vector(3 downto 0);
+	-- No clocks detected in port list. Replace <clock> below with 
+	-- appropriate port name 
+
+BEGIN
+
+	-- Instantiate the Unit Under Test (UUT)
+	uut: alu PORT MAP (
+		op_a => op_a,
+		op_b => op_b,
+		opc => opc,
+		status => status,
+		result => result
+	);
+
+	alu_test :process
+	begin
+
+		-- ADD should handle regular addition
+		op_a <= "0101"; -- 5
+		op_a <= "0110"; -- 6
+		opc <= "0000";
+		wait for 5ns;
+		assert result = "1011"; -- 11
+		assert status(2) = '0'; -- carry
+		assert status(2) = '0'; -- zero
+
+		-- ADD should handle overflow on addition
+		op_a <= "1001"; -- 9 
+		op_a <= "1000"; -- 8
+		opc <= "0000";
+		wait for 5ns;
+		assert result = "0010"; -- 2
+		assert status(2) = '1'; -- carry
+		assert status(2) = '0'; -- zero
+
+	wait;
+	end process;
+END;
+
