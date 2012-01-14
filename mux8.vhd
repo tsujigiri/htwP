4f1fa1c1dd7138872e3767325911137718946950
Helge Rausch <helge@rausch.io>
2012-01-14 12:05:29 +0100

    growing mux5 to become mux8

--- mux8.vhd
+++ mux8.vhd
@@ -2,20 +2,23 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 --use IEEE.NUMERIC_STD.ALL;
 
-entity mux5 is
+entity mux8 is
     Port ( input0 : in  STD_LOGIC_VECTOR (7 downto 0);
            input1 : in  STD_LOGIC_VECTOR (7 downto 0);
            input2 : in  STD_LOGIC_VECTOR (7 downto 0);
            input3 : in  STD_LOGIC_VECTOR (7 downto 0);
            input4 : in  STD_LOGIC_VECTOR (7 downto 0);
+           input5 : in  STD_LOGIC_VECTOR (7 downto 0);
+           input6 : in  STD_LOGIC_VECTOR (7 downto 0);
+           input7 : in  STD_LOGIC_VECTOR (7 downto 0);
            inputSel : in  STD_LOGIC_VECTOR (2 downto 0);
            output : out  STD_LOGIC_VECTOR (7 downto 0));
-end mux5;
+end mux8;
 
-architecture Behavioral of mux5 is
-
+architecture Behavioral of mux8 is
 begin
-	process( input0, input1, input2, input3, input4, inputSel )
+	process(input0, input1, input2, input3, input4,
+			  input5, input6, input7, inputSel )
 	begin
 		case inputSel is
 		when "000" => output <= input0;
@@ -23,9 +26,11 @@ begin
 		when "010" => output <= input2;
 		when "011" => output <= input3;
 		when "100" => output <= input4;
+		when "101" => output <= input5;
+		when "110" => output <= input6;
+		when "111" => output <= input7;
 		when others => output <= "11111111";
 		end case;
 	end process;
-
 end Behavioral;
 
--- mux8_test.vhd
+++ mux8_test.vhd
@@ -2,18 +2,21 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 --USE ieee.numeric_std.ALL;
  
-ENTITY mux5_test IS
-END mux5_test;
+ENTITY mux8_test IS
+END mux8_test;
  
-ARCHITECTURE behavior OF mux5_test IS 
+ARCHITECTURE behavior OF mux8_test IS 
  
-    COMPONENT mux5
+    COMPONENT mux8
     PORT(
          input0 : IN  std_logic_vector(7 downto 0);
          input1 : IN  std_logic_vector(7 downto 0);
          input2 : IN  std_logic_vector(7 downto 0);
          input3 : IN  std_logic_vector(7 downto 0);
          input4 : IN  std_logic_vector(7 downto 0);
+         input5 : IN  std_logic_vector(7 downto 0);
+         input6 : IN  std_logic_vector(7 downto 0);
+         input7 : IN  std_logic_vector(7 downto 0);
          inputSel : IN  std_logic_vector(2 downto 0);
          output : OUT  std_logic_vector(7 downto 0)
         );
@@ -25,6 +28,9 @@ ARCHITECTURE behavior OF mux5_test IS
    signal input2 : std_logic_vector(7 downto 0) := (others => '0');
    signal input3 : std_logic_vector(7 downto 0) := (others => '0');
    signal input4 : std_logic_vector(7 downto 0) := (others => '0');
+   signal input5 : std_logic_vector(7 downto 0) := (others => '0');
+   signal input6 : std_logic_vector(7 downto 0) := (others => '0');
+   signal input7 : std_logic_vector(7 downto 0) := (others => '0');
    signal inputSel : std_logic_vector(2 downto 0) := (others => '0');
 
  	--Outputs
@@ -32,23 +38,30 @@ ARCHITECTURE behavior OF mux5_test IS
 
 BEGIN
  
-   uut: mux5 PORT MAP (
+   uut: mux8 PORT MAP (
           input0 => input0,
           input1 => input1,
           input2 => input2,
           input3 => input3,
           input4 => input4,
+          input5 => input5,
+          input6 => input6,
+          input7 => input7,
           inputSel => inputSel,
           output => output
         );
 		  
-	mux5_test: process
+	mux8_test: process
 	begin
-		input0 <= "11101110";
-		input1 <= "11011101";
-		input2 <= "11001100";
-		input3 <= "10111011";
-		input4 <= "10101010";
+		
+		input0 <= "11111111";
+		input1 <= "11101110";
+		input2 <= "11011101";
+		input3 <= "11001100";
+		input4 <= "10111011";
+		input5 <= "10101010";
+		input6 <= "10011001";
+		input7 <= "10001000";
 		
 		inputSel <= "000";
 		wait for 5ns;
@@ -73,6 +86,21 @@ BEGIN
 		inputSel <= "100";
 		wait for 5ns;
 		assert output = input4;
+		
+		inputSel <= "101";
+		wait for 5ns;
+		assert output = input5;
+		wait for 5ns;
+		
+		inputSel <= "110";
+		wait for 5ns;
+		assert output = input6;
+		wait for 5ns;
+		
+		inputSel <= "111";
+		wait for 5ns;
+		assert output = input7;
+		wait for 5ns;
 		
 		wait;
 	end process;
