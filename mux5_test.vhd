de66d14d694126fe589cdd52d38dfafd5e6c1a8e
Helge Rausch <helge@rausch.io>
2012-01-14 14:05:51 +0100

    Revert "growing mux5 to become mux8"
    This reverts commit 4f1fa1c1dd7138872e3767325911137718946950.

--- mux5_test.vhd
+++ mux5_test.vhd
@@ -2,21 +2,18 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 --USE ieee.numeric_std.ALL;
  
-ENTITY mux8_test IS
-END mux8_test;
+ENTITY mux5_test IS
+END mux5_test;
  
-ARCHITECTURE behavior OF mux8_test IS 
+ARCHITECTURE behavior OF mux5_test IS 
  
-    COMPONENT mux8
+    COMPONENT mux5
     PORT(
          input0 : IN  std_logic_vector(7 downto 0);
          input1 : IN  std_logic_vector(7 downto 0);
          input2 : IN  std_logic_vector(7 downto 0);
          input3 : IN  std_logic_vector(7 downto 0);
          input4 : IN  std_logic_vector(7 downto 0);
-         input5 : IN  std_logic_vector(7 downto 0);
-         input6 : IN  std_logic_vector(7 downto 0);
-         input7 : IN  std_logic_vector(7 downto 0);
          inputSel : IN  std_logic_vector(2 downto 0);
          output : OUT  std_logic_vector(7 downto 0)
         );
@@ -28,9 +25,6 @@ ARCHITECTURE behavior OF mux8_test IS
    signal input2 : std_logic_vector(7 downto 0) := (others => '0');
    signal input3 : std_logic_vector(7 downto 0) := (others => '0');
    signal input4 : std_logic_vector(7 downto 0) := (others => '0');
-   signal input5 : std_logic_vector(7 downto 0) := (others => '0');
-   signal input6 : std_logic_vector(7 downto 0) := (others => '0');
-   signal input7 : std_logic_vector(7 downto 0) := (others => '0');
    signal inputSel : std_logic_vector(2 downto 0) := (others => '0');
 
  	--Outputs
@@ -38,30 +32,23 @@ ARCHITECTURE behavior OF mux8_test IS
 
 BEGIN
  
-   uut: mux8 PORT MAP (
+   uut: mux5 PORT MAP (
           input0 => input0,
           input1 => input1,
           input2 => input2,
           input3 => input3,
           input4 => input4,
-          input5 => input5,
-          input6 => input6,
-          input7 => input7,
           inputSel => inputSel,
           output => output
         );
 		  
-	mux8_test: process
+	mux5_test: process
 	begin
-		
-		input0 <= "11111111";
-		input1 <= "11101110";
-		input2 <= "11011101";
-		input3 <= "11001100";
-		input4 <= "10111011";
-		input5 <= "10101010";
-		input6 <= "10011001";
-		input7 <= "10001000";
+		input0 <= "11101110";
+		input1 <= "11011101";
+		input2 <= "11001100";
+		input3 <= "10111011";
+		input4 <= "10101010";
 		
 		inputSel <= "000";
 		wait for 5ns;
@@ -86,21 +73,6 @@ BEGIN
 		inputSel <= "100";
 		wait for 5ns;
 		assert output = input4;
-		
-		inputSel <= "101";
-		wait for 5ns;
-		assert output = input5;
-		wait for 5ns;
-		
-		inputSel <= "110";
-		wait for 5ns;
-		assert output = input6;
-		wait for 5ns;
-		
-		inputSel <= "111";
-		wait for 5ns;
-		assert output = input7;
-		wait for 5ns;
 		
 		wait;
 	end process;
