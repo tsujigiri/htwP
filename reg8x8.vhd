4d95aeb6e6130879b5b681f0ac67873452bce0b8
Helge Rausch <helge@rausch.io>
2012-01-14 14:04:48 +0100

    adding 8x8 register

---
+++ reg8x8.vhd
@@ -0,0 +1,57 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity reg8x8 is
+    Port ( raAddr : in  STD_LOGIC_VECTOR (2 downto 0);
+           rbPort0 : in  STD_LOGIC_VECTOR (2 downto 0);
+           rbPort1 : in  STD_LOGIC_VECTOR (2 downto 0);
+           rbSel : in  STD_LOGIC;
+           wrSrc : in  STD_LOGIC_VECTOR (1 downto 0);
+           wrPort0 : in  STD_LOGIC_VECTOR (7 downto 0);
+           wrPort1 : in  STD_LOGIC_VECTOR (7 downto 0);
+           wrPort2 : in  STD_LOGIC_VECTOR (7 downto 0);
+           wrPort3 : in  STD_LOGIC_VECTOR (7 downto 0);
+           weSel : in  STD_LOGIC_VECTOR (2 downto 0);
+           we : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           ra : out  STD_LOGIC_VECTOR (7 downto 0);
+           rb : out  STD_LOGIC_VECTOR (7 downto 0));
+end reg8x8;
+
+architecture Behavioral of reg8x8 is
+
+	-- register bank
+	type r_array is array (integer range 0 to 7) of std_logic_vector(7 downto 0);
+	signal regBank : r_array;
+	
+	signal dataIn : std_logic_vector(7 downto 0);
+	
+begin
+	ra <= regBank(to_integer(unsigned(raAddr)));
+	dataIn <= wrPort0 when wrSrc = "00"
+		  else wrPort1 when wrSrc = "01"
+		  else wrPort2 when wrSrc = "10"
+		  else wrPort3 when wrSrc = "11"
+		  else "11111111";
+
+	write: process(reset, clk)
+	begin
+		
+		if reset = '1' then
+			regBank(0) <= "00000000";
+			regBank(1) <= "00000000";
+			regBank(2) <= "00000000";
+			regBank(3) <= "00000000";
+			regBank(4) <= "00000000";
+			regBank(5) <= "00000000";
+			regBank(6) <= "00000000";
+			regBank(7) <= "00000000";
+		elsif we = '1' and rising_edge(clk) then
+			regBank(to_integer(unsigned(weSel))) <= dataIn;
+		end if;
+	end process;
+	
+end Behavioral;
+
---
+++ reg8x8_test.vhd
@@ -0,0 +1,138 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY reg8x8_test IS
+END reg8x8_test;
+ 
+ARCHITECTURE behavior OF reg8x8_test IS 
+
+    COMPONENT reg8x8
+    PORT(
+         raAddr : IN  std_logic_vector(2 downto 0);
+         rbPort0 : IN  std_logic_vector(2 downto 0);
+         rbPort1 : IN  std_logic_vector(2 downto 0);
+         rbSel : IN  std_logic;
+         wrSrc : IN  std_logic_vector(1 downto 0);
+         wrPort0 : IN  std_logic_vector(7 downto 0);
+         wrPort1 : IN  std_logic_vector(7 downto 0);
+         wrPort2 : IN  std_logic_vector(7 downto 0);
+         wrPort3 : IN  std_logic_vector(7 downto 0);
+         weSel : IN  std_logic_vector(2 downto 0);
+         we : IN  std_logic;
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         ra : OUT  std_logic_vector(7 downto 0);
+         rb : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+   --Inputs
+   signal raAddr : std_logic_vector(2 downto 0) := (others => '0');
+   signal rbPort0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal rbPort1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal rbSel : std_logic := '0';
+   signal wrSrc : std_logic_vector(1 downto 0) := (others => '0');
+   signal wrPort0 : std_logic_vector(7 downto 0) := (others => '0');
+   signal wrPort1 : std_logic_vector(7 downto 0) := (others => '0');
+   signal wrPort2 : std_logic_vector(7 downto 0) := (others => '0');
+   signal wrPort3 : std_logic_vector(7 downto 0) := (others => '0');
+   signal weSel : std_logic_vector(2 downto 0) := (others => '0');
+   signal we : std_logic := '0';
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+
+ 	--Outputs
+   signal ra : std_logic_vector(7 downto 0);
+   signal rb : std_logic_vector(7 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: reg8x8 PORT MAP (
+          raAddr => raAddr,
+          rbPort0 => rbPort0,
+          rbPort1 => rbPort1,
+          rbSel => rbSel,
+          wrSrc => wrSrc,
+          wrPort0 => wrPort0,
+          wrPort1 => wrPort1,
+          wrPort2 => wrPort2,
+          wrPort3 => wrPort3,
+          weSel => weSel,
+          we => we,
+          clk => clk,
+          reset => reset,
+          ra => ra,
+          rb => rb
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+	reg8x8_test: process
+	begin
+		reset <= '1';
+		wait for clk_period*10;
+		reset <= '0';
+		
+		wrPort0 <= "01010000";
+		wrPort1 <= "01010001";
+		wrPort2 <= "01010010";
+		wrPort3 <= "01010011";
+		
+		wrSrc <= "00";
+		weSel <= "000";
+		raAddr <= "000";
+
+		wait for clk_period;
+
+		-- write and read regBank(0)
+		assert ra = "00000000";
+		wait for clk_period;
+		we <= '1';
+		wait for clk_period;
+		assert ra = wrPort0;
+		wait for clk_period*2;
+		
+		reset <= '1';
+		wait for clk_period*2;
+		reset <= '0';
+		
+		-- write and read regBank(7)
+		assert ra = "00000000";
+		wait for clk_period;
+		raAddr <= "111";
+		wait for clk_period;
+		assert ra = "00000000";
+		wait for clk_period;
+		weSel <= "111";
+		wait for clk_period;
+		we <= '1';
+		wait for clk_period;
+		assert ra = wrPort0;
+		
+		reset <= '1';
+		wait for clk_period*2;
+		reset <= '0';
+		
+		-- wrPort2 -> regBank(5) -> ra
+		wrSrc <= "10";
+		weSel <= "101";
+		raAddr <= "101";
+		wait for clk_period;
+		assert ra = wrPort2;
+
+		wait;
+	end process;
+
+END;
